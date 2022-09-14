d28960c5f21613bb6da9f897c9d4e67029ad6e9b
Naufalammarwork <109495303+Naufalammarwork@users.noreply.github.com>
2022-09-14 16:08:51 +0700

    Change attachment

--- src/components/Portofolio/PortofolioNew.js
+++ src/components/Portofolio/PortofolioNew.js
@@ -35,7 +35,7 @@ function PortofolioNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={portofolioLink} className="d-flex justify-content-center">
+          <Document file={portofolio} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
